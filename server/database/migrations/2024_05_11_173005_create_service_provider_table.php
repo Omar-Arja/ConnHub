12c1c36abab975dfc4dc8991b465f29b1ca0302f
Omar Arja <omaralarja17@gmail.com>
2024-05-11 21:31:44 +0300

    [Server] Add migration for creating service_provider table

---
+++ server/database/migrations/2024_05_11_173005_create_service_provider_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('service_provider', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('service_name');
+            $table->string('service_description');
+            $table->string('service_category');
+            $table->string('location');
+            $table->integer('service_price_min');
+            $table->integer('service_price_max');
+            $table->string('calendly_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('service_provider');
+    }
+};
