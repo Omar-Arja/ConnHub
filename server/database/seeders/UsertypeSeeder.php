86a356e51a01386d02417a12598fd7de0ccf7abb
Omar Arja <omaralarja17@gmail.com>
2024-05-11 22:13:23 +0300

    [Server] Add UsertypeSeeder to seed user types in the database

---
+++ server/database/seeders/UsertypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Usertype;
+
+class UsertypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $usertypes = [
+            'Admin',
+            'Client',
+            'Service Provider',
+        ];
+
+        foreach ($usertypes as $usertype) {
+            Usertype::create([
+                'name' => $usertype,
+            ]);
+        }
+    }
+}
