ce921dfadf54ba5ea13dc0fdcd388a608826999a
Omar Arja <omaralarja17@gmail.com>
2024-05-15 17:15:37 +0300

    feat: Add client and service provider profile routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ServiceProviderController;
 
 Route::prefix('auth')->group(function () {
     Route::post('login', [AuthController::class, 'login']);
@@ -10,5 +12,26 @@ Route::prefix('auth')->group(function () {
     Route::middleware('auth:api')->group(function () {
         Route::post('logout', [AuthController::class, 'logout']);
         Route::post('refresh', [AuthController::class, 'refresh']);
+    });
+});
+
+Route::middleware('auth:api')->group(function () {
+    Route::prefix('client')->group(function () {
+        // By Pending
+        Route::post('profile', [ClientController::class, 'createClientProfile']);
+    });
+
+    Route::prefix('service-provider')->group(function () {
+        // By Pending
+        Route::post('profile', [ServiceProviderController::class, 'createServiceProviderProfile']);
+
+        // By ServiceProvider
+        Route::put('update', [ServiceProviderController::class, 'updateServiceProviderProfile']);
+
+        // By Client
+        Route::get('', [ServiceProviderController::class, 'getCurrentServiceProviderProfile']);
+        Route::get('/id/{id}', [ServiceProviderController::class, 'getServiceProviderProfile']);
+        Route::get('all', [ServiceProviderController::class, 'getAllServiceProviders']);
+        Route::get('search', [ServiceProviderController::class, 'searchServiceProviders']);
     });
 });
